5ac7b39d8ad697e2426b7b54e855588459c28a73
Lex <132962983+Alleexx129@users.noreply.github.com>
2024-08-07 22:57:44 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('theme') === 'dark') {
+        document.body.classList.add('dark-mode');
+    }
+});
+
 document.getElementById('theme-toggle').addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
+    localStorage.setItem('theme', document.body.classList.contains('dark-mode') ? 'dark' : 'light');
 });
